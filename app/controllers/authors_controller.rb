1e07a910a643072d18c91e733cbe45a19eec19d3
Kimani <elvis.kimani@student.moringaschool.com>
2022-09-28 15:21:43 +0300

    commit tree 985f5abf11ef3d32414273dce683ce5149f46183

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,9 @@
 class AuthorsController < ApplicationController
+  # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+ 
+ rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  
   
   def show
     author = Author.find(params[:id])
@@ -8,14 +13,24 @@ class AuthorsController < ApplicationController
 
   def create
     author = Author.create(author_params)
-
     render json: author, status: :created
+    
   end
 
   private
   
   def author_params
     params.permit(:email, :name)
+  end
+
+ 
+
+  # def render_not_found_response
+  #   render json: { error: "Author not found" }, status: :not_found
+  # end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
   
 end
