4b8420bbbc3da0e5d983bc18c0e57e2054d29a22
Kimani <elvis.kimani@student.moringaschool.com>
2022-09-28 15:21:55 +0300

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,11 @@
 class PostsController < ApplicationController
+  # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  rescue_from ActiveRecord::RecordInvalid, with: :rrender_unprocessable_entity_response
+
+  
+
+  
 
   def show
     post = Post.find(params[:id])
@@ -12,12 +19,23 @@ class PostsController < ApplicationController
     post.update(post_params)
 
     render json: post
+
   end
 
   private
 
   def post_params
     params.permit(:category, :content, :title)
+  end
+
+  
+
+  # def render_not_found_response
+  #   render json: { error: "Post not found" }, status: :not_found
+  # end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 
 end
